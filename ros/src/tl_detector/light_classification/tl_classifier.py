fddc3487c95b66433eb33dafd55354b249b7ef9c
Peng Zhao <pengzhao0524@gmail.com>
2019-10-27 15:02:13 -0700

    implement

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,10 +1,68 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
+import cv2
+import rospy
+import math
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        #self.MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
+        self.MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+        model_file = self.MODEL_NAME + '/frozen_inference_graph.pb'
 
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(model_file, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+            self.sess = tf.Session(graph=self.detection_graph)
+            # Input and output Tensors for detection_graph
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+
+    def get_average_color(self, rgb_img):
+        """Get the average value for each r g b channel in the cropped rgb image
+        """
+        val = [0,0,0]
+        for i in range(3):
+            img = rgb_img[:,:,i]
+            avg_color_per_row = np.average(img, axis=0)
+            val[i] = np.average(avg_color_per_row, axis=0)
+            
+        return val
+    
+    def determine_color(self, val):
+        """Determines the closest color 
+        red is 255, 0, 0
+        yellow is 255, 255, 0
+        green is 0, 255, 0
+        """
+        colors = [[255, 0, 0],[255, 255, 0],[0, 255, 0]]
+        diff = float("inf")
+        diff_index = -1
+        for i in range(len(colors)):
+            current_diff = (val[0]-colors[i][0])**2 + (val[1]-colors[i][1])**2 + (val[2]-colors[i][2])**2
+            if current_diff < diff:
+                diff = current_diff
+                diff_index = i
+                
+        if diff_index == 0:
+            return TrafficLight.RED
+        elif diff_index == 1:
+            return TrafficLight.YELLOW
+        elif diff_index == 2:
+            return TrafficLight.GREEN
+        else:
+            return TrafficLight.UNKNOWN
+            
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
 
@@ -16,4 +74,51 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_np = np.asarray(image, dtype="uint8")
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        
+        detected = False
+
+        with self.detection_graph.as_default():
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+                feed_dict={self.image_tensor: image_np_expanded})
+        boxes = np.squeeze(boxes)
+        classes = np.squeeze(classes).astype(np.int32)
+        scores = np.squeeze(scores)
+        best_scores = []
+        
+        for idx, classID in enumerate(classes):
+            if self.MODEL_NAME == 'ssd_mobilenet_v1_coco_2017_11_17':
+                if classID == 10: # 10 is traffic light
+                    if scores[idx] > 0.15: #confidence level
+                        best_scores.append([scores[idx], idx, classID])
+                        detected = True
+        
+        if detected:
+            # Sort to get the best score
+            best_scores.sort(key=lambda tup: tup[0], reverse=True)
+            best_score = best_scores[0]
+            nbox = boxes[best_score[1]]
+            # Get bounding box for this object
+            height = image.shape[0]
+            width = image.shape[1]
+            box = np.array([nbox[0]*height, nbox[1]*width, nbox[2]*height, nbox[3]*width]).astype(int)
+            cropped_image = image[box[0]:box[2], box[1]:box[3]]
+            # Get average color 
+            val = self.get_average_color(cropped_image)
+            # Predict color
+            traffic_light = self.determine_color(val)
+            # Draw bounding box with detected color
+            traffic_light_color = (255,255,255)#black 
+            if traffic_light==TrafficLight.RED:
+                traffic_light_color = (255,0,0)
+            elif traffic_light == TrafficLight.YELLOW:
+                traffic_light_color = (255,255,0)
+            elif traffic_light == TrafficLight.GREEN:
+                traffic_light_color = (0,255,0)
+            cv2.rectangle(image, (box[1],box[0]), (box[3],box[2]),traffic_light_color,4)
+            return traffic_light
+            
         return TrafficLight.UNKNOWN
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -5,22 +5,26 @@ from geometry_msgs.msg import PoseStamped, Pose
 from styx_msgs.msg import TrafficLightArray, TrafficLight
 from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
 import yaml
+from scipy.spatial import KDTree
 
 STATE_COUNT_THRESHOLD = 3
+SKIP_THRESHOLD = 4
 
 class TLDetector(object):
     def __init__(self):
-        rospy.init_node('tl_detector')
+        rospy.init_node('tl_detector',log_level=rospy.INFO)
 
         self.pose = None
         self.waypoints = None
         self.camera_image = None
         self.lights = []
+        self.waypoints_2d = None
+        self.waypoint_tree = None
 
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -32,8 +36,8 @@ class TLDetector(object):
         simulator. When testing on the vehicle, the color state will not be available. You'll need to
         rely on the position of the light and the camera image to predict it.
         '''
-        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
-        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
+        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb, queue_size=1)
+        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb, queue_size=1)
 
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
@@ -48,14 +52,20 @@ class TLDetector(object):
         self.last_state = TrafficLight.UNKNOWN
         self.last_wp = -1
         self.state_count = 0
+        self.skip_count = 0
 
         rospy.spin()
 
     def pose_cb(self, msg):
+        #rospy.loginfo("pose_cb")
         self.pose = msg
 
     def waypoints_cb(self, waypoints):
         self.waypoints = waypoints
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
+
 
     def traffic_cb(self, msg):
         self.lights = msg.lights
@@ -68,6 +78,14 @@ class TLDetector(object):
             msg (Image): image from car-mounted camera
 
         """
+        #rospy.loginfo("new image come in")
+        # Skip (SKIP_THRESHOLD - 1) images for a small latency
+        self.skip_count += 1
+        self.skip_count = self.skip_count % SKIP_THRESHOLD
+        if self.skip_count != 0:
+            #rospy.loginfo('\nSkip Image: %d', self.skip_count)
+            return
+        
         self.has_image = True
         self.camera_image = msg
         light_wp, state = self.process_traffic_lights()
@@ -83,14 +101,15 @@ class TLDetector(object):
             self.state = state
         elif self.state_count >= STATE_COUNT_THRESHOLD:
             self.last_state = self.state
-            light_wp = light_wp if state == TrafficLight.RED else -1
+            if state != TrafficLight.RED:
+                light_wp = -1
             self.last_wp = light_wp
-            self.upcoming_red_light_pub.publish(Int32(light_wp))
+            self.upcoming_red_light_pub.publish(Int32(self.last_wp))
         else:
             self.upcoming_red_light_pub.publish(Int32(self.last_wp))
         self.state_count += 1
 
-    def get_closest_waypoint(self, pose):
+    def get_closest_waypoint(self, x, y):
         """Identifies the closest path waypoint to the given position
             https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
         Args:
@@ -101,7 +120,8 @@ class TLDetector(object):
 
         """
         #TODO implement
-        return 0
+        closest_idx = self.waypoint_tree.query([x,y], 1)[1]
+        return closest_idx
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -115,12 +135,23 @@ class TLDetector(object):
         """
         if(not self.has_image):
             self.prev_light_loc = None
-            return False
+            return TrafficLight.UNKNOWN
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         #Get classification
-        return self.light_classifier.get_classification(cv_image)
+        light_state = self.light_classifier.get_classification(cv_image)
+        traffic_light_color = 'UNKNOWN'
+        if light_state == TrafficLight.RED:
+            traffic_light_color = 'RED'
+        elif light_state == TrafficLight.YELLOW:
+            traffic_light_color = 'YELLOW'
+        elif light_state == TrafficLight.GREEN:
+            traffic_light_color = 'GREEN'
+        rospy.loginfo('\nDetection: %s', traffic_light_color)
+        #rospy.loginfo('\nDetection: %d', light_state)
+        return light_state
+
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
@@ -131,19 +162,38 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        light = None
+        closest_light = None
+        line_wp_idx = None
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
-        if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
+        #if not self.pose:
+            #rospy.loginfo('pose is None')
+        #if not self.waypoints:
+            #rospy.loginfo('waypoints are None')
+        if self.pose and self.waypoints:
+            car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
+        else:
+            car_wp_idx = 0
 
         #TODO find the closest visible traffic light (if one exists)
+        diff = len(self.waypoints.waypoints)
+        for i, light in enumerate(self.lights):
+            # Get stop line waypoint index
+            line = stop_line_positions[i]
+            temp_wp_idx = self.get_closest_waypoint(line[0], line[1])
+            # Find closest stop line waypoint index
+            d = temp_wp_idx - car_wp_idx
+            if d >= 0 and d < diff:
+                diff = d 
+                closest_light = light
+                line_wp_idx = temp_wp_idx
+                    
+        rospy.loginfo('diff: %d', diff)
+        if closest_light and diff < 100: 
+            state = self.get_light_state(closest_light)
+            return line_wp_idx, state
 
-        if light:
-            state = self.get_light_state(light)
-            return light_wp, state
-        self.waypoints = None
         return -1, TrafficLight.UNKNOWN
 
 if __name__ == '__main__':
